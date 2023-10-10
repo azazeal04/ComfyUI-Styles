67a9b123c0f537da94edd7a0e2381e1844719290
azazeal04 <132445160+azazeal04@users.noreply.github.com>
2023-10-10 16:24:31 +0200

    Have changed the nodes. There is now separate normal and advanced nodes, as well as a single normal and advanced node named ComfyUI styler with all the styles.

--- __init__.py
+++ __init__.py
@@ -1,25 +1,3 @@
-from .Aesthetic_Styler.Aesthetic_Styler import *
-from .Anime_Styler.Anime_Styler import *
-from .Fantasy_Styler.Fantasy_Styler import *
-from .Gothic_Styler.Gothic_Styler import *
-from .Halloween_Styler.Halloween_Styler import *
-from .Line_Art_Styler.Line_Art_Styler import *
-from .Movie_Poster_Styler.Movie_Poster_Styler import *
-from .Punk_Styler.Punk_Styler import *
-from .Travel_Poster_Styler.Travel_Poster_Styler import *
-
-NODE_CLASS_MAPPINGS = {
-    "Aesthetic Styler": AestheticStyler,
-    "Anime Styler": AnimeStyler,
-    "Fantasy Styler": FantasyStyler,
-    "Gothic Styler": GothicStyler,
-    "Halloween Styler": HalloweenStyler,
-    "Line Art Styler": LineArtStyler,
-    "Movie Poster_Styler": MoviePosterStyler,
-    "Punk Style": PunkStyler,
-    "Travel Poster Styler": TravelPosterStyler,
-}
+from .sdxl_prompt_styler import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
-
-print("\033[34mAzaeal: \033[92mLoaded\033[0m")
